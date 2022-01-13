8c4c31fbe35d145acf2c0f196f66c088f3928000
men1692 <men2961@naver.com>
2022-01-13 22:26:23 +0900

    추가 문제풀이

---
+++ src/inflearn/p3_3.java
@@ -0,0 +1,32 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class p3_3 {
+	
+	public int solution(int n, int k, int [] arr) {
+		int answer, sum = 0;
+		
+		for(int i = 0; i < k; i++) sum += arr[i];
+		answer = sum;
+		for(int i = k; i < n; i++) {
+			sum += (arr[i] - arr[i-k]);
+			answer = Math.max(answer, sum);
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		p3_3 T = new p3_3();
+		Scanner kb = new Scanner(System.in);
+		
+		int n = kb.nextInt();
+		int k = kb.nextInt();
+		
+		int [] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = kb.nextInt();
+		}
+		System.out.println(T.solution(n,  k, arr));
+	}
+}
